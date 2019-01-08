b01f544de856b2527027815583617fd7d9b50995
Yari PL <yaroslav.khudchenko@gmail.com>
2019-01-08 23:52:44 +0100

    added buttons

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { ButtonToolbar, MenuItem, DropdownButton } from 'react-bootstrap';
 
 class Box extends React.Component {
 	//must be arrow function to refer to good this
@@ -52,6 +53,46 @@ class Grid extends React.Component {
 				{rowsArr}
 			</div>
 		);
+	}
+}
+class Buttons extends React.Component {
+	handleSelect = (evt) => {
+		this.props.gridSize (evt);
+	}
+	render() {
+		return (
+			<div className='center'>
+				<ButtonToolbar>
+					<button className="btn btn-default" onClick={this.props.playButton}>
+						Play
+					</button>
+					<button className="btn btn-default" onClick={this.props.pauseButton}>
+					  Pause
+					</button>
+					<button className="btn btn-default" onClick={this.props.clear}>
+					  Clear
+					</button>
+					<button className="btn btn-default" onClick={this.props.slow}>
+					  Slow
+					</button>
+					<button className="btn btn-default" onClick={this.props.fast}>
+					  Fast
+					</button>
+					<button className="btn btn-default" onClick={this.props.seed}>
+					  Seed
+					</button>
+					<DropdownButton
+						title="Grid Size"
+						id="size-menu"
+						onSelect={this.handleSelect}
+					>
+						<MenuItem eventKey="1">20x10</MenuItem>
+						<MenuItem eventKey="2">50x30</MenuItem>
+						<MenuItem eventKey="3">70x50</MenuItem>
+					</DropdownButton>
+				</ButtonToolbar>
+			</div>
+		)
 	}
 }
 
@@ -146,6 +187,15 @@ class Main extends React.Component {
 			//should be always one top div(or other element)
 			<div>
 				<h1>The Game of Life</h1>
+				<Buttons 
+					playButton={this.playButton}
+					pauseButton={this.pauseButton}
+					slow={this.slow}
+					fast={this.fast}
+					clear={this.clear}
+					seed={this.seed}
+					gridSize={this.gridSize}
+				/>
 				<Grid 
 					gridFull = {this.state.gridFull}
 					rows = {this.rows}
